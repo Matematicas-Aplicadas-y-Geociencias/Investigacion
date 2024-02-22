a2a9317eaa69e8a8fe5b7964f02524e768bf9e2e
JC Cajas <carlos.cajas@enesmerida.unam.mx>
2024-02-22 11:16:55 -0600

    Empaquetamiento en m\'odulos de las subrutinas que ensamblan las matrices tridiagonales del problema de calor 2D

--- OpenACC/calor2DAcc/constantes.f90
+++ OpenACC/calor2DAcc/constantes.f90
@@ -1,5 +1,98 @@
 module constantes
-    integer, parameter :: mi = 2000, nj=1000, nn = 1200000
-    real, parameter    :: pi = 3.1415926535
-    INTEGER, PARAMETER :: DBL=SELECTED_REAL_KIND(P=15,R=300)
+  integer, parameter :: mi = 100, nj=100, nn = 1200000
+  real, parameter    :: pi = 3.1415926535
+  INTEGER, PARAMETER :: DBL=SELECTED_REAL_KIND(P=15,R=300)
+  contains
+    
+    subroutine ensambla_tdmax(AIo,ACo,ADo,resultxo,deltaxo,deltayo,temp_anto,cond_tero,temp_inio,temp_fino,jjo)
+      !$acc routine
+      implicit none
+      double precision, intent(in)                    :: deltaxo,deltayo,cond_tero
+      double precision, intent(in)                    :: temp_inio,temp_fino
+      double precision, dimension(mi,nj), intent(in)  :: temp_anto
+      
+      double precision, dimension(mi,nj), intent(out) :: AIo,ADo
+      double precision, dimension(mi,nj), intent(out) :: ACo
+      double precision, dimension(mi,nj), intent(out) :: resultxo
+
+      integer :: iin,jjo
+
+      !
+      ! Se definen las condiciones de frontera
+      !
+      ACo(1,jjo)       = 1.0d0
+      ADo(1,jjo)       = 0.0d0
+      resultxo(1,jjo)  = temp_inio
+      AIo(mi,jjo)      = 0.d0
+      ACo(mi,jjo)      = 1.0d0
+      resultxo(mi,jjo) = temp_fino
+      !
+      ! Ensamblado de la matriz tridiagonal
+      ! y del vector de resultados
+      !
+      do iin=2, mi-1
+         AIo(iin,jjo)      =-1.0d0*cond_tero/(deltaxo*deltaxo)
+         ACo(iin,jjo)      = 2.0d0*cond_tero*(1.d0/(deltaxo*deltaxo)+1.d0/(deltayo*deltayo))
+         ADo(iin,jjo)      =-1.0d0*cond_tero/(deltaxo*deltaxo)
+         resultxo(iin,jjo) = cond_tero/(deltayo*deltayo)*temp_anto(iin,jjo+1)+&
+              &cond_tero/(deltayo*deltayo)*temp_anto(iin,jjo-1)
+      end do
+      
+    end subroutine ensambla_tdmax
+
+    subroutine ensambla_tdmay(BIo,BCo,BDo,resultyo,deltaxo,deltayo,tempero,cond_tero,flux_abao,flux_arro,iio)
+      !$acc routine
+      implicit none
+      double precision, intent(in)                      :: deltaxo,deltayo,cond_tero
+      double precision, intent(in)                      :: flux_arro,flux_abao
+      double precision, dimension(mi,nj), intent(in)    :: tempero
+      
+      double precision, dimension(mi,nj), intent(out)   :: BIo,BDo
+      double precision, dimension(mi,nj), intent(out)   :: BCo
+      double precision, dimension(mi,nj), intent(out)   :: resultyo
+      integer :: iio,jjn
+      !
+      ! Se definen las condiciones de frontera
+      !
+      BCo(1,iio)       =-1.0d0/deltayo
+      BDo(1,iio)       = 1.0d0/deltayo
+      resultyo(1,iio)  = 0.d0 !flux_abao
+      BIo(nj,iio)      =-1.d0/deltayo
+      BCo(nj,iio)      = 1.0d0/deltayo
+      resultyo(nj,iio) = 0.d0 !flux_arro !(temp_fin+temp_ini)/2.d0
+      !
+      ! Ensamblado de la matriz tridiagonal
+      ! y del vector de resultados
+      !
+      do jjn=2, nj-1
+         BIo(jjn,iio)     =-1.0d0*cond_tero/(deltayo*deltayo)
+         BCo(jjn,iio)     = 2.0d0*cond_tero*(1.d0/(deltayo*deltayo)+1.d0/(deltaxo*deltaxo))
+         BDo(jjn,iio)     =-1.0d0*cond_tero/(deltayo*deltayo)
+         resultyo(jjn,iio) = cond_tero/(deltaxo*deltaxo)*tempero(iio+1,jjn)+&
+              &cond_tero/(deltaxo*deltaxo)*tempero(iio-1,jjn)
+      end do
+    end subroutine ensambla_tdmay
+    
+    subroutine tri(a,b,c,r,u,n)
+      !$acc routine
+      implicit none
+      integer, intent(in) :: n
+      double precision, intent(in) :: a(n),c(n)
+      double precision, intent(inout) :: b(n),r(n)
+      double precision, intent(inout) :: u(n)
+      integer :: i
+      
+      u=0.d0
+      ! eliminacion elementos bajo la matriz
+      gausselim: do i=2,n
+         r(i)=r(i)-(a(i)/b(i-1))*r(i-1)
+         b(i)=b(i)-(a(i)/b(i-1))*c(i-1)
+      end do gausselim
+      ! solucion para u
+      u(n)=r(n)/b(n)
+      sustatras: do i=n-1,1,-1
+         u(i)=(r(i)-c(i)*u(i+1))/b(i)
+      end do sustatras
+    end subroutine tri
+    
 end module
--- OpenACC/calor2DAcc/ecCalor2D_tdma_parallel.f90
+++ OpenACC/calor2DAcc/ecCalor2D_tdma_parallel.f90
@@ -1,7 +1,6 @@
 PROGRAM Calor2D
 !
 use constantes
-use trian
 implicit none !(type, external)
 ! external :: sgesv
 integer :: ii, jj, kk
@@ -11,32 +10,25 @@ double precision :: a, b, deltax, deltay
 !
 ! Parámetros físicos del problema
 double precision :: cond_ter, temp_ini, temp_fin
-double precision :: flux_aba, flux_arr
+double precision :: flux_aba, flux_arr, alpha
 !
 ! Arreglos para incógnitas, término fuente y posición
 double precision, dimension(mi)    :: sourcex, xx
 double precision, dimension(nj)    :: sourcey, yy
-double precision, dimension(mi)    :: resultx,tempx
-double precision, dimension(mi,nj)    :: resultxo,tempxo
-double precision, dimension(nj)    :: resulty,tempy
-double precision, dimension(nj,mi)    :: resultyo,tempyo
+double precision, dimension(mi,nj) :: resultx
+double precision, dimension(mi,nj) :: resulty,tempy
 double precision, dimension(mi,nj) :: temper,temp_ant
 !
 ! Matriz a invertir
-double precision, dimension(mi)    :: AI,AD
-double precision, dimension(mi)    :: AC
-double precision, dimension(nj)    :: BI,BD
-double precision, dimension(nj)    :: BC
-
-double precision, dimension(mi,nj)    :: AIo,ADo
-double precision, dimension(mi,nj)    :: ACo
-double precision, dimension(nj,mi)    :: BIo,BDo
-double precision, dimension(nj,mi)    :: BCo
+double precision, dimension(mi,nj) :: AI,AD
+double precision, dimension(mi,nj) :: AC
+double precision, dimension(mi,nj) :: BI,BD
+double precision, dimension(mi,nj) :: BC
 !
 ! Se crea la malla 2D
 !
-a = 10.d0
-b = 5.d0
+a = 1.d0
+b = 5.d-1
 do ii = 1, mi
     xx(ii) = a*dfloat(ii-1)/dfloat(mi-1)
 !     print*, ii, xx(ii)
@@ -49,125 +41,82 @@ deltay = 1.d0/dfloat(nj-1)
 !
 ! Se definen los parámetros físicos del problema
 !
-cond_ter = 97.5d-6
-temp_ant = 300.d0
+cond_ter = 1.d0
 temp_ini = 308.d0
 temp_fin = 298.d0
 flux_aba = 0.d0
 flux_arr = 0.d0
+alpha    = 0.5d0 ! par\'ametro de relajaci\'on
 !
 ! Inicialización de los arreglos a utilizar
 !
 AI = 0.d0; AC = 0.d0; AD = 0.d0
 BI = 0.d0; BC = 0.d0; BD = 0.d0
 temper = (temp_fin+temp_ini)/2.d0
+temp_ant = temper
 resultx = 0.0d0
 resulty = 0.0d0
 !
-!
-! Se muestra el máximo número permitido de hilos
-    !max_proc = omp_get_max_threads()
-    !print*, "El máximo número de hilos permitido es: ", max_proc
-!
-! Se obtiene el identificador de hilo
-    !id = omp_get_thread_num()
-    !print*, " Yo soy el hilo: ", id
-!
-!! Bucle iterativo
-!
-do kk = 1, 100
-    !
-    !
-    !!! Abrimos la región paralela
-    !
-    !
-    !
-    !!! PARTE 1
-    !
-    !
-    ! Inicia el ciclo que recorre la coordenada y resolviendo
-    ! problemas 1D en la dirección de x
-    !$acc parallel
-    !$acc loop
-    do jj = 2, nj-1
-        !
-        ! Se definen las condiciones de frontera
-        !
-        ACo(1,jj)      = 1.0d0
-        ADo(1,jj)      = 0.0d0
-        resultx(1)  = temp_ini
-        AIo(mi,jj)     = 0.d0
-        ACo(mi,jj)     = 1.0d0
-        resultx(mi) = temp_fin
-        !
-        ! Ensamblado de la matriz tridiagonal
-        ! y del vector de resultados
-        !
-        do ii=2, mi-1
-            AIo(ii,jj)     =-1.0d0*cond_ter/(deltax*deltax)
-            ACo(ii,jj)     = 2.0d0*cond_ter*(1.d0/(deltax*deltax)+1.d0/(deltay*deltay))
-            ADo(ii,jj)     =-1.0d0*cond_ter/(deltax*deltax)
-            resultxo(ii,jj) = cond_ter/(deltay*deltay)*temp_ant(ii,jj+1)+cond_ter/(deltay*deltay)*temp_ant(ii,jj-1)
-        end do
-        !
-        ! Llamamos al TDMA
-        call tri(AIo(:,jj),ACo(:,jj),ADo(:,jj),resultxo(:,jj),temper(:,jj),mi)
-!         do ii =1, mi
-!             temper(ii,jj) = tempxo(ii,jj)
-!         end do
+! Bucle de pseudotiempo
+! y
+do kk = 1, 500
+   !
+   ! Abrimos la región paralela
+   !
+!!$   !$acc parallel
+   !
+   ! Inicia el ciclo que recorre la coordenada y resolviendo
+   ! problemas 1D en la dirección de x
+   
+   !$acc parallel loop
+   do jj = 2, nj-1
+      !
+      ! Ensamblamos matrices en direcci\'on x
+      !
+      call ensambla_tdmax(AI,AC,AD,resultx,deltax,deltay,temp_ant,cond_ter,temp_ini,temp_fin,jj)
+      !
+      ! Llamamos al TDMA
+      call tri(AI(:,jj),AC(:,jj),AD(:,jj),resultx(:,jj),temper(:,jj),mi)
+      
     end do
-    !$acc end loop
-    !$acc end parallel
-    !     !
-!     !
-
-    !!! PARTE 2
-    !
+!!$    !$acc end loop
     !
     ! Inicia el ciclo que recorre la coordenada x resolviendo
     ! problemas 1D en la dirección de y
     !
-    !$acc parallel
-    !$acc loop
+    !$acc parallel loop
     do ii = 2, mi-1
-        !
-        ! Se definen las condiciones de frontera
-        !
-        BCo(1,ii)      =-1.0d0/deltay
-        BDo(1,ii)      = 1.0d0/deltay
-        resultyo(1,ii) = flux_aba
-        BIo(nj,ii)     =-1.d0/deltay
-        BCo(nj,ii)     = 1.0d0/deltay
-        resultyo(nj,ii) = flux_arr !(temp_fin+temp_ini)/2.d0
-        !
-        ! Ensamblado de la matriz tridiagonal
-        ! y del vector de resultados
-        !
-        do jj=2, nj-1
-            BIo(jj,ii)     =-1.0d0*cond_ter/(deltay*deltay)
-            BCo(jj,ii)     = 2.0d0*cond_ter*(1.d0/(deltay*deltay)+1.d0/(deltax*deltax))
-            BDo(jj,ii)     =-1.0d0*cond_ter/(deltay*deltay)
-            resultyo(jj,ii) = cond_ter/(deltax*deltax)*temp_ant(ii+1,jj)+cond_ter/(deltax*deltax)*temp_ant(ii-1,jj)
-        end do
+       !
+       ! Ensamblamos matrices tridiagonales en direcci\'on y
+       !
+       call ensambla_tdmay(BI,BC,BD,resulty,deltax,deltay,temper,cond_ter,flux_aba,flux_arr,ii)
         !
         ! Llamamos al TDMA
         !
-        call tri(BIo(:,ii),BCo(:,ii),BDo(:,ii),resultyo(:,ii),tempyo(:,ii),nj)
+        call tri(BI(:,ii),BC(:,ii),BD(:,ii),resulty(:,ii),tempy(:,ii),nj)
         do jj =1, nj
-            temper(ii,jj) = tempyo(jj,ii)
+            temper(ii,jj) = tempy(jj,ii)
         end do
-    end do
-    !$acc end loop
-    !$acc end parallel
-!     !
-!     !
 
-    !!! Cerramos la región paralela
+    end do
+!!$    !$acc end loop
     !
-    !print*, "Residuo: ", kk, maxval(temper-temp_ant)
+    ! se aplica un esquema de relajaci\'on fija
+    !
+    temper = alpha*temper+(1.d0-alpha)*temp_ant
+    !
+    ! se actualiza la temperatura de la iteraci\'on anterior
+    !
     temp_ant = temper
     !
-end do
+    ! Cerramos la región paralela
+    !
+!!$    !$acc end parallel
+    !print*, "Residuo: ", kk, maxval(temper-temp_ant)
+!     temp_ant = temper
+    !
+    end do
+
     !
     !! Escritura de resultados
     !

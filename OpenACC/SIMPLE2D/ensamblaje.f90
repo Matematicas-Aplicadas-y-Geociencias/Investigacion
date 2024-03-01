3168c767331602c59567ab991c85bf0981091d9c
JC Cajas <carlos.cajas@enesmerida.unam.mx>
2024-03-01 17:54:27 -0600

    avance en la modularizaci\'on del programa SIMPLE2D para trasladarlo a openacc

--- OpenACC/SIMPLE2D/ensamblaje.f90
+++ OpenACC/SIMPLE2D/ensamblaje.f90
@@ -1,26 +1,40 @@
+!
+!
+! M\'odulo de ensamblaje
+!
+! Este m\'odulo contiene las subrutinas que calculan los coeficientes de las matrices
+! tridiagonales a invertir en el barrido l\'inea por l\'inea
+!
+!
 MODULE ensamblaje
   use constantes
 
 contains
-  subroutine ensambla_velx(yo,fey,d_xuo,d2_xuo,d_yvo,u_o,u_anto,v_o,&
+  !*******************************************************************
+  !
+  ! ensambla_velux
+  !
+  ! Subrutina que calcula los coeficientes de la matriz tridiagonal
+  ! para la velocidad u barriendo la direcci\'on x
+  !
+  !*******************************************************************
+  subroutine ensambla_velux(yo,fey,d_xuo,d2_xuo,d_yvo,u_o,u_anto,v_o,&
        &temp_o,pres_o,gamma_uo,Ri_o,dt_o,du_o,au_o,rel_vo,&
-       AIx,ACx,ADx,Rx)
+       AI,ACi,AD,Rxi)
     implicit none
     integer :: i,j,k,info
     !**********************************
     ! Variables del flujo e inc'ognitas
-    real(kind=DBL), dimension(mi,nj+1), intent(in)    :: u_o
-    real(kind=DBL), dimension(mi,nj+1), intent(out)   :: au_o,du_o
-    real(kind=DBL), dimension(mi,nj+1), intent(in)    :: u_anto,gamma_uo,Ri_o
-    real(kind=DBL), dimension(mi+1,nj), intent(in)    :: v_o
-    real(kind=DBL), dimension(mi+1,nj+1), intent(in)  :: temp_o,pres_o
-    real(kind=DBL), dimension(mi,nj+1) :: fu
+    real(kind=DBL), dimension(mi,nj+1),   intent(in)    :: u_o
+    real(kind=DBL), dimension(mi,nj+1),   intent(out)   :: au_o
+    real(kind=DBL), dimension(mi,nj+1),   intent(in)    :: u_anto,gamma_uo,Ri_o
+    real(kind=DBL), dimension(mi+1,nj),   intent(in)    :: v_o
+    real(kind=DBL), dimension(mi+1,nj+1), intent(in)    :: temp_o,pres_o
+    real(kind=DBL), dimension(mi,nj+1)                  :: fu
     !**********************************
     !Variables de la tridiagonal
     real(kind=DBL), dimension(mi),   intent(out) :: ACi,Rxi
     real(kind=DBL), dimension(mi-1), intent(out) :: AI,AD
-    real(kind=DBL), dimension(nj+1)              :: ACj,Ryj
-    real(kind=DBL), dimension(nj)                :: AS,AN
     !*********************
     !Variables de interpolaci'on
     real(kind=DBL) :: ui,ud,vn,vs,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n
@@ -34,20 +48,18 @@ contains
     !********************
     !Variables auxiliares
     real(kind=DBL) :: alpha,beta
-    !*************************
-    !auxiliar para calcular du
-    fu = u_o
     !********************
     !TDMA en direccion x
     !$OMP  PARALLEL DO DEFAULT(NONE)&
-    !$OMP& PRIVATE(AI,ACi,AD,Rxi,ui,ud,vs,vn,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n,delta_x,delta_y,temp_int,alpha,beta,info)&
-    !$OMP& SHARED(u_o,fu,u_anto,pres_o,gamma_uo,au_o,v_o,d_xuo,d2_xuo,yo,fey,d_yvo,temp_o,rel_vo,Ri_o,dt_o)
+    !$OMP& PRIVATE(AI,ACi,AD,Rxi,ui,ud,vs,vn,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n,&
+    !$OMP& delta_x,delta_y,temp_int,alpha,beta,info)&
+    !$OMP& SHARED(u_o,fu,u_anto,pres_o,gamma_uo,au_o,v_o,d_xuo,d2_xuo,yo,fey,d_yvo,temp_o,&
+    !$OMP& rel_vo,Ri_o,dt_o)
     do j = 2, nj
        !***********************
        !Condiciones de frontera
        ACi(1) = 1._DBL
-       AD(1)  = cero
-
+       AD(1)  = 0._DBL
        Rxi(1) = 1._DBL !-6.d0*dfloat(j-1)/dfloat(nj)*(dfloat(j-1)/dfloat(nj)-1.d0)! 1._DBL !
        au_o(1,j) = 1.e40_DBL !ACi(1)
        !*************************
@@ -69,7 +81,7 @@ contains
           gamma_n = 1._DBL/((1._DBL-fey(j+1)) / gamma_uo(i,j) + fey(j+1) / gamma_uo(i,j+1))
           delta_x = d2_xuo(i)/2._DBL
           delta_y = d_yvo(j-1)
-          temp_int = (1._DBL - di/(2._DBL*delta_x)) * temp_o(i,j) + di/(2._DBL*delta_x) * temp_o(i+1,j)
+          temp_int = (1._DBL-di/(2._DBL*delta_x))*temp_o(i,j)+di/(2._DBL*delta_x)*temp_o(i+1,j)
           !     (temp_o(i,j)+temp_o(i+1,j))/2._DBL
           !************************
           AI(i-1) =-(gamma_i*delta_y/di*DMAX1(cero,(1._DBL-0.1_DBL*dabs(ui*di/gamma_i))**5._DBL)+&
@@ -86,12 +98,124 @@ contains
                &ACi(i)*(1._DBL-rel_vo)*u_o(i,j)
           au_o(i,j) = ACi(i) * rel_vo
        end do
+       !***********************
+       !Condiciones de frontera
+       ACi(mi)  = 1._DBL
+       AI(mi-1) =-1._DBL !
+       Rxi(mi)  = cero
+       au_o(mi,j) = 1.e40_DBL !ACi(mi)
     end do
     
-  end subroutine ensambla_velx
+  end subroutine ensambla_velux
 
-  subroutine ensambla_vely(xo,fex,d_yvo,d2_yvo,d_xuo,u_o,v_o,v_anto,&
-       &pres_o,gamma_vo,dt_o,dv_o,av_o,rel_vo)
+
+  !*******************************************************************
+  !
+  ! ensambla_veluy
+  !
+  ! Subrutina que calcula los coeficientes de la matriz tridiagonal
+  ! para la velocidad u barriendo la direcci\'on y
+  !
+  !*******************************************************************
+  subroutine ensambla_veluy(yo,fey,d_xuo,d2_xuo,d_yvo,u_o,u_anto,v_o,&
+       &temp_o,pres_o,gamma_uo,Ri_o,dt_o,du_o,au_o,rel_vo,&
+       AIuy,ACuy,ADuy,Ruy)
+    !**********************************
+    ! Variables del flujo e inc'ognitas
+    real(kind=DBL), dimension(mi,nj+1),   intent(in)    :: u_o
+    real(kind=DBL), dimension(mi,nj+1),   intent(out)   :: au_o
+    real(kind=DBL), dimension(mi,nj+1),   intent(in)    :: u_anto,gamma_uo,Ri_o
+    real(kind=DBL), dimension(mi+1,nj),   intent(in)    :: v_o
+    real(kind=DBL), dimension(mi+1,nj+1), intent(in)    :: temp_o,pres_o
+    real(kind=DBL), dimension(mi,nj+1)                  :: fu
+    !**********************************
+    !Variables de la tridiagonal
+    real(kind=DBL), dimension(nj+1),intent(out)         :: ACj,Ryj
+    real(kind=DBL), dimension(nj),  intent(out)         :: AS,AN
+    !*********************
+    !Variables de interpolaci'on
+    real(kind=DBL) :: ui,ud,vn,vs,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n
+    real(kind=DBL) :: delta_x,delta_y,temp_int
+    !*********************
+    !Variables de la malla,volumen de control,incremento de tiempo y num Richardson
+    real(kind=DBL), dimension(nj+1), intent(in) :: yo,fey
+    real(kind=DBL), dimension(mi-1), intent(in) :: d_xuo,d2_xuo
+    real(kind=DBL), dimension(nj-1), intent(in) :: d_yvo
+    real(kind=DBL), intent(in) :: dt_o,rel_vo
+    !********************
+    !Variables auxiliares
+    real(kind=DBL) :: alpha,beta
+
+    !$OMP  PARALLEL DO DEFAULT(NONE)&
+    !$OMP& PRIVATE(AS,ACj,AN,Ryj,ui,ud,vs,vn,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,&
+    !$OMP& gamma_n,delta_x,delta_y,temp_int,alpha,beta,info)&
+    !$OMP& SHARED(gamma_uo,au_o,u_o,u_anto,pres_o,v_o,d_xuo,d2_xuo,&
+    !$OMP& yo,fey,d_yvo,temp_o,rel_vo,Ri_o,dt_o)
+    DO i = 2, mi-1
+       !***********************
+       !Condiciones de frontera
+       ACj(1) = 1._DBL
+       AN(1)  = cero
+       Ryj(1) = cero
+       au_o(i,1) =1.e40_DBL !ACj(1)
+       !*************************
+       !Llenado de la matriz en y
+       DO j = 2, nj
+          !**************************
+          !Interpolaciones necesarias
+          ui = (u_o(i-1,j)+u_o(i,j))/2._DBL
+          ud = (u_o(i,j)+u_o(i+1,j))/2._DBL
+          vs = v_o(i,j-1)+d_xuo(i-1)/d2_xuo(i)*(v_o(i+1,j-1)-v_o(i,j-1))
+          vn = v_o(i,j)+d_xuo(i-1)/d2_xuo(i)*(v_o(i+1,j)-v_o(i,j))
+          di = d_xuo(i-1)
+          dd = d_xuo(i)
+          ds = yo(j)-yo(j-1)
+          dn = yo(j+1)-yo(j)
+          gamma_i = 2._DBL * gamma_uo(i-1,j) * gamma_uo(i,j) / (gamma_uo(i-1,j) + gamma_uo(i,j))
+          gamma_d = 2._DBL * gamma_uo(i+1,j) * gamma_uo(i,j) / (gamma_uo(i+1,j) + gamma_uo(i,j))
+          gamma_s = 1._DBL/((1._DBL-fey(j)) / gamma_uo(i,j-1) + fey(j) / gamma_uo(i,j))
+          gamma_n = 1._DBL/((1._DBL-fey(j+1)) / gamma_uo(i,j) + fey(j+1) / gamma_uo(i,j+1))
+          delta_x = d2_xuo(i)/2._DBL
+          delta_y = d_yvo(j-1)
+          temp_int= (1._DBL-di/(2._DBL*delta_x))*temp_o(i,j)+di/(2._DBL*delta_x)*temp_o(i+1,j)
+          !     (temp_o(i,j)+temp_o(i+1,j))/2._DBL
+          !************************
+          alpha  = gamma_i*delta_y/di*DMAX1(cero,(1._DBL-0.1_DBL*dabs(ui*di/gamma_i))**5._DBL)+&
+               &DMAX1(cero,ui*delta_y)
+          beta   = gamma_d*delta_y/dd*DMAX1(cero,(1._DBL-0.1_DBL*dabs(ud*dd/gamma_d))**5._DBL)+&
+               &DMAX1(cero,-ud*delta_y)
+          AS(j-1)=-(gamma_s*delta_x/ds*DMAX1(cero,(1._DBL-0.1_DBL*dabs(vs*ds/gamma_s))**5._DBL)+&
+               &DMAX1(cero,vs*delta_x))
+          AN(j)  =-(gamma_n*delta_x/dn*DMAX1(cero,(1._DBL-0.1_DBL*dabs(vn*dn/gamma_n))**5._DBL)+&
+               &DMAX1(cero,-vn*delta_x))
+          ACj(j) = (alpha+beta-AS(j-1)-AN(j)+delta_x*delta_y/dt_o) / rel_vo
+          Ryj(j) = alpha*u_o(i-1,j)+beta*u_o(i+1,j)-delta_x*delta_y*Ri_o(i,j)*temp_int+&
+               &delta_x*delta_y*u_anto(i,j)/dt_o+(pres_o(i,j)-pres_o(i+1,j))*delta_y+&
+               &ACj(j)*(1._DBL-rel_vo)*u_o(i,j)
+          au_o(i,j) = ACj(j) * rel_vo
+       END DO
+       !***********************
+       !Condiciones de frontera
+       ACj(nj+1) = 1._DBL
+       AS(nj)    = cero
+       Ryj(nj+1) = cero
+       au_o(i,nj+1) = 1.e40_DBL !ACj(nj+1)
+       !*************************
+    end DO
+  end subroutine ensambla_veluy
+
+
+  !*******************************************************************
+  !
+  ! ensambla_velvy
+  !
+  ! Subrutina que calcula los coeficientes de la matriz tridiagonal
+  ! para la velocidad v barriendo la direcci\'on y
+  !
+  !*******************************************************************
+  subroutine ensambla_velvy(xo,fex,d_yvo,d2_yvo,d_xuo,u_o,v_o,v_anto,&
+       &pres_o,gamma_vo,dt_o,dv_o,av_o,rel_vo,&
+       )
     IMPLICIT NONE
     INTEGER :: i,j,k,info
     !**********************************
@@ -126,7 +250,8 @@ contains
     !********************
     !TDMA en direccion x
     !$OMP  PARALLEL DO DEFAULT(NONE)&
-    !$OMP& PRIVATE(AI,ACi,AD,Rxi,ui,ud,vs,vn,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n,delta_x,delta_y,alpha,beta,info)&
+    !$OMP& PRIVATE(AI,ACi,AD,Rxi,ui,ud,vs,vn,di,dd,ds,dn,gamma_i,gamma_d,gamma_s,gamma_n,&
+    !$OMP& delta_x,delta_y,alpha,beta,info)&
     !$OMP& SHARED(v_o,fv,v_anto,pres_o,gamma_vo,av_o,u_o,d_xuo,d2_yvo,xo,fex,d_yvo,rel_vo,dt_o)
     DO j = 2, nj-1
        !***********************

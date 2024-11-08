ae070bba6cf96c78214354ee9becb9172607c6cf
Edwin Enrique <edwin.perez@enesmerida.unam.mx>
2024-11-08 16:13:24 -0600

    Implementar CMAKE
    Se realizan los ajustes para implementar cmake.

---
+++ OpenACC/integrador_3d/source/integrador_3d.f90
@@ -0,0 +1,18 @@
+subroutine integrador_3d()
+
+    use mod_constantes
+
+    implicit none
+
+    real(kind=DBL), intent(in) a, b, c, d, e
+    real(kind=DBL), intent(in) x0, x2, y0, y1
+    real(kind=DBL), intent(out) integral
+
+    term_a = (a/3) * (x2**3 - x0**3) (y2 - y0)
+    term_b = (b/3) * (x2 - x0) (y2**3 - y0**3)
+    term_c = (c/2) * (x2**2 - x0**2) (y2 - y0)
+    term_d = (d/2) * (x2 - x0) (y2**2 - y0**2)
+    term_e = e * (x2 - x0) (y2 - y0)
+
+    integral = term_a + term_b + term_c + term_d + term_e
+end subroutine integrador_3d
